e6196fd82cd293abf09d12daa189b29734b1db7b
Olga <fmwmf@ukr.net>
2025-06-08 14:02:34 +0300

    New

--- src/main/java/org/dailyfarm/security/config/CorsConfig.java
+++ src/main/java/org/dailyfarm/security/config/CorsConfig.java
@@ -1,12 +1,12 @@
 package org.dailyfarm.security.config;
 
-import java.util.List;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 public class CorsConfig {
@@ -14,7 +14,10 @@ public class CorsConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:3000"));
+        config.setAllowedOrigins(List.of(
+                "http://localhost:3000",
+                "https://daily-farm.vercel.app"
+        ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true); // разрешить куки (с httpOnly)
